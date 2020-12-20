9e398d399513dc8ae2390bf92c585e4a71309444
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-20 16:04:19 +0100

    updated heapsort

--- Algorithms/Sorting/heapsort.py
+++ Algorithms/Sorting/heapsort.py
@@ -1,6 +1,6 @@
 '''
-Heapsort implementation
-@author André Gilbert, andre.gilbert.77110@gmail.com
+Heapsort implementation O(n * log(n))
+@author: André Gilbert, andre.gilbert.77110@gmail.com
 '''
 from typing import List
 
