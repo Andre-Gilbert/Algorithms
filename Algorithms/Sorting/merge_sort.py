9b609d803a26450d36138dffbb09dfc7f91f63b6
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-20 17:05:44 +0100

    updated merge sort

--- Algorithms/Sorting/merge_sort.py
+++ Algorithms/Sorting/merge_sort.py
@@ -7,14 +7,26 @@ from typing import List
 
 class MergeSort:
     def sort(self, array: List[int]) -> List[int]:
+        """Merge sort is the algorithm which follows divide and conquer approach. 
+        
+        If array Contains 0 or 1 elements then it is already sorted, otherwise, 
+        Divide array into two sub-array of equal number of elements.
+        Conquer means sort the two sub-arrays recursively using the merge sort.
+        Combine the sub-arrays to form a single final sorted array maintaining the ordering of the array.
+        The main idea behind merge sort is that, the short list takes less time to be sorted.
+        """
         n = len(array)
         if n <= 1: return array
 
+        # Split array into two parts and recursively sort them
         left = self.sort(array[:n // 2])
         right = self.sort(array[n // 2:])
+
+        # Combine the two arrays into one larger array
         return self.__merge(left, right)
 
     def __merge(self, left: List[int], right: List[int]) -> List[int]:
+        """Merge two sorted arrays into a larger sorted array."""
         left_length = len(left)
         right_length = len(right)
         n = left_length + right_length
