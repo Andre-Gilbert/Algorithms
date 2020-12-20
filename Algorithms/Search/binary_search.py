e5dae34ed1c6cbe6e8cd7f874e92ca3d19a79234
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-20 16:46:38 +0100

    updated binary search

--- Algorithms/Search/binary_search.py
+++ Algorithms/Search/binary_search.py
@@ -7,6 +7,12 @@ from typing import List
 
 class BinarySearch:
     def binary_search_iterative(self, array: List[int], target: int) -> int:
+        """Binary search looks for a particular item by comparing the middle most item of the collection.
+        
+        If the middle item is greater than the item, 
+        then the item is searched in the sub-array to the left of the middle item. 
+        Otherwise, the item is searched for in the sub-array to the right of the middle item.
+        """
         if len(array) == 0: return
         low = 0
         hi = len(array) - 1
@@ -34,6 +40,7 @@ class BinarySearch:
             return self.binary_search_recursive(array, target, mid + 1, hi)
 
 
+# Example usage
 if __name__ == '__main__':
     array = [-1, 1, 2, 4, 5, 20, 22, 34, 134, 379]
     bs = BinarySearch()
