f11306498a8d014904bcfcd4a73de7381fd130c7
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-28 17:26:03 +0100

    updated linear_search.py

--- Algorithms/Search/linear_search.py
+++ Algorithms/Search/linear_search.py
@@ -2,22 +2,50 @@
 Linear search implementation - O(n)
 @author: André Gilbert, andre.gilbert.77110@gmail.com
 '''
+
 from typing import List
 
 
 class LinearSearch:
     def search(self, array: List[int], target: int) -> int:
-        """Returns the index of the value that we are searching for."""
+        """Returns the index of the value that we are searching for.
+
+        This method is more memory efficient because it does not 
+        create additional stack frames for recursive functions calls.
+
+        Args:
+            array: A list of integer.
+            target: An integer value to search.
+            
+        Returns:
+            An integer representing the index of the target otherwise None.
+            
+            Example:
+            >>> search([0, 5, 7, 10, 15], 6)
+            -1
+            >>> search([0, 5, 7, 10, 15], 15)
+            4
+            >>> search([0, 5, 7, 10, 15], 0)
+            0
+        """
+
         if not array: return
         for i, element in enumerate(array):
             if element == target:
                 return i
         return -1
 
+    def search_recursive(self, array: List[int], target: int, index: int = 0) -> int:
+        if index > len(array) - 1: return -1
+
+        if array[index] == target:
+            return index
+        else:
+            return self.search_recursive(array, target, index + 1)
+
 
 # Example usage
 if __name__ == '__main__':
-    array = [10, 2, 24, 12, 34, 1, 3, 2, 1, 79]
     ls = LinearSearch()
-    index_of_34 = ls.search(array, 34)
-    print(index_of_34)
+    print(f"Position of 34 in the array: {ls.search([10, 2, 24, 12, 34, 1, 3, 2, 1, 79], 34)}")
+    print(f"Position of 12 in the array: {ls.search_recursive([10, 2, 24, 12, 34, 1, 3, 2, 1, 79], 0)}")
