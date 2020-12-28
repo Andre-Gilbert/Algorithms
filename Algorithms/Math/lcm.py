699c5049dd6d4b0c1fce10f641da4ee30d291dbe
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-28 16:33:27 +0100

    updated lcm

--- Algorithms/Math/lcm.py
+++ Algorithms/Math/lcm.py
@@ -6,7 +6,22 @@ An implementation of finding the LCM of two numbers - O(log(a + b))
 
 class LCM:
     def lcm(self, a: int, b: int) -> float:
-        """Compute the lowest common multiple of a and b."""
+        """Compute the least common multiple of a and b.
+        Args: 
+            a: An integer.
+            b: An integer.
+
+        Returns:
+            A float representing the lcm of a and b.
+
+            Example:
+            >>> lcm(12, 76)
+            228
+            >>> lcm(5, 2)
+            10
+            >>> lcm(12, 18)
+            36
+        """
         lcm = (a / self.__gcd(a, b)) * b
         return lcm if lcm > 0 else -lcm
 
@@ -18,6 +33,5 @@ class LCM:
 # Example usage
 if __name__ == "__main__":
     find = LCM()
-    print(find.lcm(12, 18))
-    print(find.lcm(5, 2))
-    print(find.lcm(72, 56))
+    print(f"Least Common Multiple of 12 & 18 = {find.lcm(12, 18)}")
+    print(f"Least Common Multiple of 3 & 10 = {find.lcm(3, 10)}")
