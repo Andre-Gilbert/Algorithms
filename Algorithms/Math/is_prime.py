c60fb17b22ad23c9819861b93e45f49b38d81469
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-28 16:56:58 +0100

    updated is_prime.py

--- Algorithms/Math/is_prime.py
+++ Algorithms/Math/is_prime.py
@@ -3,16 +3,35 @@ Tests whether a number is a prime number or not - O(sqrt(n))
 @author: André Gilbert, andre.gilbert.77110@gmail.com
 '''
 
-from math import sqrt, floor
+from math import sqrt
 
 
 class IsPrime:
-    def is_prime_iterative(self, n: int) -> bool:
+    def is_prime(self, n: int) -> bool:
+        """Checks to see if a number is a prime.
+
+        A number is prime if it has exactly two factors: 1 and itself.
+
+        Args: 
+            n: An integer.
+
+        Returns:
+            True if n is a prime number otherwise False.
+
+            Example:
+            >>> is_prime(2)
+            True
+            >>> is_prime(3)
+            True
+            >>> is_prime(4)
+            False
+        """
+
         if n < 2: return False
         if (n == 2 or n == 3): return True
         if (n % 2 == 0 or n % 3 == 0): return False
 
-        limit = floor(sqrt(n))
+        limit = int(sqrt(n))
 
         for i in range(5, limit + 1):
             if (n % i == 0 or n % (i + 2) == 0):
@@ -32,10 +51,5 @@ class IsPrime:
 
 if __name__ == "__main__":
     find = IsPrime()
-    print(find.is_prime_iterative(5))
-    print(find.is_prime_iterative(31))
-    print(find.is_prime_iterative(1433))
-    print("----")
-    print(find.is_prime_recursive(5))
-    print(find.is_prime_recursive(31))
-    print(find.is_prime_recursive(1433))
+    print(f"1433 is a prime number: {find.is_prime(1433)}")
+    print(f"1231 is a prime number: {find.is_prime_recursive(1231)}")
