bb045fe78db28071058d3ecb027c55bab6469ed1
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-28 17:03:51 +0100

    updated relatively_prime.py

--- Algorithms/Math/relatively_prime.py
+++ Algorithms/Math/relatively_prime.py
@@ -6,7 +6,26 @@ Test to see whether two numbers are relatively prime - O(log (a + b))
 
 class RelativelyPrime:
     def are_coprime(self, a: int, b: int) -> bool:
-        """Two numbers are relatively prime if greatest common factor is 1."""
+        """Checks whether two number are coprime.
+        
+        Two numbers are relatively prime if greatest common factor is 1.
+        
+        Args:
+            a: An integer.
+            b: An integer.
+            
+        Returns:
+            True if two numbers are relatively prime otherwise False.
+            
+            Example:
+            >>> are_coprime(12, 18)
+            False
+            >>> are_coprime(9, 3)
+            False
+            >>> are_coprime(3, 7)
+            True
+        """
+
         return self.__gcf(a, b) == 1
 
     def __gcf(self, a: int, b: int) -> int:
@@ -17,5 +36,5 @@ class RelativelyPrime:
 # Example usage
 if __name__ == "__main__":
     check = RelativelyPrime()
-    print(check.are_coprime(12, 6))
-    print(check.are_coprime(3, 7))
+    print(f"12 & 6 are relatively prime: {check.are_coprime(12, 6)}")
+    print(f"10 & 3 are relatively prime: {check.are_coprime(10, 3)}")
