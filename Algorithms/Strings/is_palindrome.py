480dc4ea32d814f19ec0451c90836a3049a547a1
André Gilbert <andre.gilbert.77110@gmail.com>
2020-12-30 17:39:56 +0100

    updated is_palindrome.py

---
+++ Algorithms/Strings/is_palindrome.py
@@ -0,0 +1,38 @@
+'''
+Test to see whether the string is a palindrome - O(n)
+@author: André Gilbert, andre.gilbert.77110@gmail.com
+'''
+
+
+class Palindrome:
+    def is_palindrome(self, s: str) -> bool:
+        """Determines whether a string is a palindrome or not.
+        
+        A string s is a palindrome when it reads the same forward as backward.
+
+        Args:
+            s: A String.
+            
+        Returns:
+            True if given string is a palindrome otherwise False.
+
+            Examples:
+            >>> is_palindrome("Hello")
+            False
+            >>> is_palindrome("Able was I ere I saw Elba")
+            True
+            >>> is_palindrome("racecar")
+            True
+            >>> is_palindrome("Mr. Owl ate my metal worm?")
+            True
+        """
+
+        s = "".join([char for char in s.lower() if char.isalnum()])
+        return s == s[::-1]
+
+
+# Example usage
+if __name__ == "__main__":
+    palindrome = Palindrome()
+    s = "a man a plan a canal panama"
+    print(palindrome.is_palindrome(s))
